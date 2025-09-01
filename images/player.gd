abf65f64a470195aeb461681ae50f83af2b6d507
Berk <apetbert378@gmail.com>
2025-09-01 21:40:29 +0300

    .

---
+++ images/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+const SPEED = 1400.0
+const JUMP_VELOCITY = -1700.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+	# Ölüm kontrolü
+	if position.y > 1000:  # sahnenin alt sınırına göre ayarla
+		get_tree().reload_current_scene()
